02e8dcfb17a2d3153efe4eb955e8e75b08ca060a
Will McCudden <wmccudden-hughes@qa.com>
2021-01-13 14:48:37 +0000

    Added all files necessary for dealing with orders, added to ims and started working on create order

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -1,5 +1,52 @@
 package com.qa.ims.persistence.domain;
+import java.text.SimpleDateFormat;
 
 public class Order {
+	
+	private int order_id;
+	private int customer_id;
+	private String address;
+	
+	public Order(int order_id, int customer_id, String address) {
+		super();
+		this.order_id = order_id;
+		this.customer_id = customer_id;
+		this.address = address;
+	}
+
+	public Order(int customer_id, String address) {
+		super();
+		this.customer_id = customer_id;
+		this.address = address;
+	}
+
+	public int getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+
+	public int getCustomer_id() {
+		return customer_id;
+	}
+
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+	
+	
 
 }
