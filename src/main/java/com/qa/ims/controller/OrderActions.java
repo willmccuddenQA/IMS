059fd705864d755e24d8648b694f53183bac1252
Will McCudden <wmccudden-hughes@qa.com>
2021-01-14 12:48:28 +0000

    added OrderActions.java file for actions related to orders

---
+++ src/main/java/com/qa/ims/controller/OrderActions.java
@@ -0,0 +1,48 @@
+package com.qa.ims.controller;
+
+import org.apache.log4j.Logger;
+
+import com.qa.ims.utils.Utils;
+
+public enum OrderActions {
+	
+	ADD("Add item to order"), DELETEITEM("Delete item from order"), CALCULATE("Calculate order cost"),
+	DELETE("Removes an order from the database"), READITEMS("Read items from order"), 
+	CREATE("Add new order to database"), READ("Read all orders from database"),
+	ADDITEM("Add an item to and order"), RETURN("return to domain selection");
+	
+	public static final Logger LOGGER = Logger.getLogger(Action.class);
+	
+	private String description;
+	
+	private OrderActions() {
+		
+	}
+	
+	OrderActions(String description){
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return this.name() + ": " + this.description;
+	}
+	
+	public static void printActions() {
+		for(Action action : Action.values()) {
+			LOGGER.info(action.getDescription());
+		}
+	}
+	
+	public static Action getAction() {
+		Action action;
+		while(true) {
+			try {
+				action = Action.valueOf(Utils.getInput().toUpperCase());
+				break;
+			} catch (IllegalArgumentException e){
+				LOGGER.error("Invalid selection please try again");
+			}
+		}
+		return action;
+	}
+}
